d5ec21e9bb66013ebdc2106a1eca3be42bff8cd7
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2023-08-02 00:16:21 +0545

    file changes

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -6,8 +6,8 @@ var tokenData = JSON.parse(token);
 console.log('client ')
 
 let httpclient = axios.create({
-  baseURL: 'http://localhost:9000/api/v1/',
-  // baseURL: 'https://admin-dashboard-backend-8yg4.onrender.com/api/v1',
+  // baseURL: 'http://localhost:9000/api/v1/',
+  baseURL: 'https://change-backend.onrender.com/api/v1',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
